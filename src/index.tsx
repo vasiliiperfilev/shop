60ffcb3d68ba1b6ae3955e3063cf84a49fc51d46
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-06-22 10:07:48 -0700

    Add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,39 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { unstable_HistoryRouter as HistoryRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import LandingBanner from './components/LandingBanner';
 import ShopCardsGalery from './components/ShopCardsGalery';
 import ItemPage from './pages/ItemPage';
 import ShopPage from './pages/ShopPage';
+import { store } from './redux/store';
+import { Provider } from 'react-redux'
+import ErrorPage from './pages/ErrorPage';
+import history from './utils/history';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+    <HistoryRouter history={history}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<LandingBanner />} />
           <Route path="shop" element={<ShopPage />}>
+            <Route index element={<ShopCardsGalery />} />
             <Route path=":categoryName" element={<ShopCardsGalery />} />
             <Route path=":categoryName/:itemId" element={<ItemPage />} />
           </Route>
+          <Route
+            path="/error"
+            element={<ErrorPage />}
+          />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HistoryRouter>
+    </Provider>
   </React.StrictMode>
 );
